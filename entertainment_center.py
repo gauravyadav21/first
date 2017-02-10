d140ad30a1798ea19257e7ccfa2317dbc5b9ecfa
gauravyadav21 <gaurav217yadav@gmail.com>
2017-02-10 12:57:45 +0530

    Add files via upload

---
+++ entertainment_center.py
@@ -0,0 +1,7 @@
+import media
+
+kaabil = media.Movie("Kaabil",
+		     "story of a blind man taking revenge of his wife dead",
+		     "http://www.lyricsted.com/wp-content/uploads/2016/12/kaabil-hoon-lyrics-title-song-hrithik-roshan-yami-gautam.jpg"
+		     "https://www.youtube.com/watch?v=nubDFeiUAsI")
+kaabil.show_trailer()
